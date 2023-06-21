8a9500374a6508479bbb5195e621d4018d4bf75a
Agustina Martínez <68882235+agustinamartinez1044@users.noreply.github.com>
2023-06-21 00:27:54 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <agustinamartinez1044>
+# agustinamartinez1044
 # Input:
 # p, principal amount
 # t, time period in years
